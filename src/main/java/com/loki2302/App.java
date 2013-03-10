cacbacc96e737c3a8d07a31eff6c9ddb628391d1
loki2302 <2302johnsmith@gmail.com>
2013-03-10 11:17:36 -0700

    Minor tweaks

--- src/main/java/com/loki2302/App.java
+++ src/main/java/com/loki2302/App.java
@@ -8,18 +8,18 @@ import com.loki2302.evaluation.FailureReason;
 import com.loki2302.evaluation.ProgramResult;
 import com.loki2302.parser.Parser;
 import com.loki2302.parser.ParserResult;
+import com.loki2302.program.Statement;
 
 public class App {
     public static void main(String[] args) {
     	Parser parser = new Parser();
-    	ParserResult parserResult = parser.parse("123;");
+    	ParserResult parserResult = parser.parse("123;222;");
     	if(!parserResult.ok) {
     		System.out.println("parser says it's bad");
     		return;
     	}
     	
-    	System.out.println("parser says it's ok");
-    	
+    	System.out.println("parser says it's ok:");    	
     	DOMProgram domProgram = parserResult.program;
     	for(DOMStatement statement : domProgram.getStatements()) {
     		System.out.println(statement);
@@ -28,13 +28,17 @@ public class App {
     	Injector injector = Guice.createInjector();
     	DOMProgramEvaluator domProgramEvaluator = injector.getInstance(DOMProgramEvaluator.class);
     	
-    	ProgramResult result = domProgramEvaluator.evaluateDOMProgram(domProgram);
-    	System.out.printf("result ok: %b\n", result.ok);
-    	if(result.ok) {
-    		System.out.println(result.program);
-    	} else {
+    	ProgramResult result = domProgramEvaluator.evaluateDOMProgram(domProgram);    	
+    	if(!result.ok) {
+    		System.out.println("evaluator says it's bad:");
     		for(FailureReason failureReason : result.failureReasons) {
     			System.out.println(failureReason);
+    		}
+    		return;
+    	} else {
+    		System.out.println("evaluator says it's ok:");
+    		for(Statement statement : result.program.getStatements()) {
+    			System.out.println(statement);
     		}
     	}
     }
